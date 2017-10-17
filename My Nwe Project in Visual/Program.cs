c045534ab942d216f759b72d80a29ffbd6e7bcb1
CCNA <CCNA@CCNA-PC>
2017-10-17 12:57:36 -0700

    Add project files.

---
+++ My Nwe Project in Visual/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Nwe_Project_in_Visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.(" Hello Worled");
+            Console.ReadKey();
+        }
+    }
+}
